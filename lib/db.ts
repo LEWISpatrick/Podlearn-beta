ae88b0d5dfacde52aa19aced88d03764c59cdf97
LEWISpatrick <133691910+LEWISpatrick@users.noreply.github.com>
2024-10-04 11:47:25 -0300

    fixed the db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  // eslint-disable-next-line no-var
-  var db: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const db = globalThis.db || new PrismaClient();
+export const db =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
 
-if (process.env.NODE_ENV !== "production") globalThis.db = db;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
